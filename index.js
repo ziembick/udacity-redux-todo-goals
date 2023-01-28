20a43fde08953f48a14b599338811a0249d62988
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-28 19:34:45 -0300

    update

--- index.js
+++ index.js
@@ -25,13 +25,54 @@ function createStore(reducer) {
   };
 }
 // App Code
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+
 function todos(state = [], action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map((todo) =>
         todo.id !== action.id
           ? todo
@@ -41,28 +82,50 @@ function todos(state = [], action) {
       return state;
   }
 }
-
 function goals(state = [], action) {
   switch (action.type) {
-    case "ADD_GOAL":
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case "REMOVE_GOAL":
+    case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id);
     default:
       return state;
   }
 }
-
-const store = createStore(todos);
-
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+const store = createStore(app);
 store.subscribe(() => {
   console.log("The new state is: ", store.getState());
 });
-store.dispatch({
-  type: "ADD_TODO",
-  todo: {
+
+store.dispatch(
+  addTodoAction({
     id: 0,
-    name: "Learn Redux",
+    name: "Walk the dog",
     complete: false,
-  },
-});
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 1,
+    name: "Wash the car",
+    complete: false,
+  })
+);
+
+store.dispatch(
+  addTodoAction({
+    id: 2,
+    name: "Go to the gym",
+    complete: true,
+  })
+);
+store.dispatch(removeTodoAction(1));
+store.dispatch(toggleTodoAction(0));
+store.dispatch(addGoalAction({}));
