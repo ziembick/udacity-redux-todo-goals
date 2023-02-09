9370621231fe24a37e28bbc59a1d90a2b8639e8e
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-09 15:10:24 -0300

    checker middleware

---
+++ udacity-todos-goals/src/middleware/checker.js
@@ -0,0 +1,27 @@
+import { ADD_GOAL } from "../actions/goals";
+import { ADD_TODO } from "../actions/todos";
+
+export const checker = (store) => (next) => (action) => {
+  if (action.type === ADD_GOAL) {
+    alert("That's a great goal!");
+  }
+
+  if (action.type === ADD_TODO) {
+    alert(`Don't forget to ${action.todo.name}`);
+  }
+
+  if (
+    action.type === ADD_TODO &&
+    action.todo.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+  if (
+    action.type === ADD_GOAL &&
+    action.goal.name.toLowerCase().includes("bitcoin")
+  ) {
+    return alert("Nope. That's a bad idea.");
+  }
+
+  return next(action);
+};
